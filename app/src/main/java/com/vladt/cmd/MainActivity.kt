5bc04abb7a79c09e05ce719267db5788ee738d64
Vladislav Tribilo <vladislav.tribilo@gmail.com>
2019-02-06 10:13:01 +0300

    add avatar view

--- app/src/main/java/com/vladt/cmd/MainActivity.kt
+++ app/src/main/java/com/vladt/cmd/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.vladt.cmd
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.welcome_page_layout)
+
+        val imageView: ImageView = findViewById(R.id.avatar)
+
+        val bitmap: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.avatar)
+        val roundedBitmapDrawable: RoundedBitmapDrawable = RoundedBitmapDrawableFactory.create(resources, bitmap)
+        roundedBitmapDrawable.isCircular = true
+        imageView.setImageDrawable(roundedBitmapDrawable)
     }
 }
